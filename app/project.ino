8477bd69aaf6c510f53735ff9bf62d1bdca4482e
Gabriel Dantas <107351005+GDantas11@users.noreply.github.com>
2025-07-25 21:13:23 -0300

    refactor(project): rename variables and defines tab sizes.
    - create 'heating' function for calculate tempeture in celsius degrees.

--- app/project.ino
+++ app/project.ino
@@ -2,89 +2,86 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-#define pinoTermistor 13
-#define pinoCartucho 8
-#define pinoCartuchoAux 10
+#define THERMISTOR_PIN 13
+#define CARTRIDGE_PIN 8
+#define AUX_CARTRIDGE_PIN 10
+#define DRIVER_1_PIN 6
+#define DRIVER_2_PIN 5
+#define DRIVER_SPEED_PIN 4
 
-#define Vin 5.0
-#define T0 298.15
-#define Rt 4700.0
-#define R0 100000.0
-
+float default_voltage = 5.0;
+float initial_temp = 298.15;
+float serie_resistance = 4700.0;
+float initial_resistance_therm = 100000.0;
 float beta = 3950.0;
-float Rinf = 0.0;
-float TempKelvin = 0.0;
-float TempCelsius = 0.0;
-float Vout = 0.0;
-float Rout = 0.0;
-
-//motor_A
-int IN1 = 6 ;
-int IN2 = 5 ;
-int velocidadeA = 4;
-
-int vel = 100;
+float aux_resistance = 0.0;
+float kelvin_temp_voltage = 0.0;
+float celsius_temp = 0.0;
+float voltage = 0.0;
+float resistance_therm = 0.0;
+int seep_pwm = 100;
 
 void setup(){
+	Serial.begin(9600);
 
-  Serial.begin(9600);
+	lcd.init();
+	lcd.backlight();
+	lcd.setCursor(0, 0);
+	lcd.print("Grupo Extrusora");
+	lcd.setCursor(0, 1);
+	lcd.print("SPD Engenharia");
 
-  // LCD
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Grupo Extrusora");
-  lcd.setCursor(0, 1);
-  lcd.print("SPD Engenharia");
+	aux_resistance = initial_resistance_therm * exp(-beta / initial_temp);
 
-  Rinf = R0 * exp(-beta / T0);
+	pinMode(CARTRIDGE_PIN, OUTPUT);
+	pinMode(AUX_CARTRIDGE_PIN, OUTPUT);
+	pinMode(DRIVER_1_PIN, OUTPUT);
+	pinMode(DRIVER_2_PIN, OUTPUT);
+	pinMode(DRIVER_SPEED_PIN, OUTPUT);
 
-  // Cartucho de aquecimento
-  pinMode(pinoCartucho, OUTPUT);
-  pinMode(pinoCartuchoAux, OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(velocidadeA,OUTPUT);
-
-  delay(2000);
+	delay(1000);
 }
 
 void loop(){
+	Serial.print("Temperatura: ");
+	Serial.print(celsius_temp);
+	Serial.print(" C, Resistencia: ");
+	Serial.println(resistance_therm);
 
-  Vout = Vin * ((float)(analogRead(pinoTermistor)) / 1023.0);
+	lcd.clear();
+	lcd.setCursor(0, 0);
+	lcd.print("Temperatura");
 
-  Rout = (Rt * Vout / (Vin - Vout));
-  TempKelvin = (beta / log(Rout / Rinf));
-  TempCelsius = TempKelvin - 273.15;
+	digitalWrite(DRIVER_1_PIN, HIGH);
+	digitalWrite(DRIVER_2_PIN, LOW);
+	analogWrite(DRIVER_SPEED_PIN, seep_pwm);
 
-  // Imprime os dados no monitor serial, podemos excluir
-  Serial.print("Temperatura: ");
-  Serial.print(TempCelsius);
-  Serial.print(" C, Resistencia: ");
-  Serial.println(Rout);
-// Exibe no LCD
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Temperatura");
+	heating();
 
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
+	if (celsius_temp > 180.0) {
+		lcd.setCursor(0, 1);
+		lcd.print("ideal atingida");
 
-  digitalWrite(velocidadeA, vel);
+		digitalWrite(CARTRIDGE_PIN, LOW);
+		digitalWrite(AUX_CARTRIDGE_PIN, LOW);
+	}
+	else {
+		String strTempCelcius = String(celsius_temp, 1) + String((char)223) + "C";
 
-  if (TempCelsius > 180.0) {
-    lcd.setCursor(0, 1);
-    lcd.print("ideal atingida");
+		digitalWrite(CARTRIDGE_PIN, HIGH);
+		digitalWrite(AUX_CARTRIDGE_PIN, HIGH);
 
-    digitalWrite(pinoCartucho, LOW);
-    digitalWrite(pinoCartuchoAux, LOW);
-  } else {
-    String strTempCelcius = String(TempCelsius, 1) + String((char)223) + "C";
-    digitalWrite(pinoCartucho, HIGH);
-    digitalWrite(pinoCartuchoAux, HIGH);
-    lcd.setCursor(0, 1);
-    lcd.print(strTempCelcius);
-  }
+		lcd.setCursor(0, 1);
+		lcd.print(strTempCelcius);
+	}
 
-  delay(1000);
+	delay(1000);
+}
+
+void heating(){
+	voltage = default_voltage * ((float)(analogRead(THERMISTOR_PIN)) / 1023.0);
+
+	resistance_therm = (serie_resistance * voltage / (default_voltage - voltage));
+	kelvin_temp_voltage = (beta / log(resistance_therm / aux_resistance));
+	celsius_temp = kelvin_temp_voltage - 273.15;
 }
